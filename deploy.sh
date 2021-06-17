a1f30b4b9030a412f12d1e6a42ea00add421059c
samyo0 <samuel.j.yoo@gmail.com>
2021-06-17 00:49:14 -0400

    deploy 7

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@ docker build -t masbot/multi-worker:latest -t masbot/multi-worker:$SHA -f ./work
 docker push masbot/multi-client:latest
 docker push masbot/multi-server:latest
 docker push masbot/multi-worker:latest
+
 docker push masbot/multi-client:$SHA
 docker push masbot/multi-server:$SHA
 docker push masbot/multi-worker:$SHA
 
 kubectl apply -f k8s
-
-kubectl set image deployments/server-deployments server=masbot/multi-server:$SHA
-kubectl set image deployments/client-deployments client=masbot/multi-client:$SHA
-kubectl set image deployments/worker-deployments worker=masbot/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=masbot/multi-server:$SHA
+kubectl set image deployments/client-deployment client=masbot/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=masbot/multi-worker:$SHA
